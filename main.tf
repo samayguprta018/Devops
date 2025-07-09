8024a888543f8626f291e7ffcdd885da6386172a
samayguprta018 <samaygupta054@gmail.com>
2025-07-09 19:15:06 +0530

    Update main.tf
    fixed

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = jenkins
+  version         = "20.37.1" 
+  cluster_name    = "jenkins"
   cluster_version = "1.29"
-  subnets         = subnet-0b03c9c53f1d5fffa
-  vpc_id          = vpc-0780cc803a9b17876
+  subnet_ids      = ["subnet-0b03c9c53f1d5fffa"]
+  vpc_id          = "vpc-0780cc803a9b17876"
   enable_irsa     = true
-  manage_aws_auth = true
 
-  node_groups = {
+  eks_managed_node_groups = {
     eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
 
       instance_types = ["t3.medium"]
     }
   }
 }
-
